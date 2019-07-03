46835a479fb09bc2f163a86c5905f562630a49df
Irina <ivira000@gmail.com>
2019-07-03 16:30:35 +0300

    Ex 3 Не работает.
    Прошу помочь разобраться в ошибке.

--- src/Example.java
+++ src/Example.java
@@ -10,13 +10,15 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class Example {
     private AppiumDriver driver;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName", "Android");
@@ -32,8 +34,7 @@ public class Example {
     }
 
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         driver.quit();
     }
 
@@ -41,8 +42,8 @@ public class Example {
     public void exampleTestXpath() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
-        "not find Search Box",
-        15
+                "not find Search Box",
+                15
         );
 
         waitForElementAndSendKeys(
@@ -69,7 +70,7 @@ public class Example {
     }
 
     @Test
-    public void ex2FindSearch(){
+    public void ex2FindSearch() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "not find Search Box",
@@ -79,34 +80,82 @@ public class Example {
                 By.xpath("//*[contains(@text, 'Search…')]"),
                 "Cannot find Search input",
                 15);
+
     }
 
-    private WebElement waitForElementPresents(By by, String error_massage, long timeoutSeconds)
-    {
-        WebDriverWait wait = new WebDriverWait(driver,timeoutSeconds);
+
+    @Test
+    public void ex3FindWordAndCancelSearch() throws InterruptedException {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "not find Search Box",
+                15
+        );
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Leonardo da Vinci",
+                "Cannot find Search input",
+                15);
+
+
+        waitForElementPresents(
+                By.xpath("//*[(@resource-id = 'org.wikipedia:id/page_list_item_container')]"),
+                "not find",
+                10
+        );
+
+        int c = 0;
+        c = driver.findElements(By.xpath("//*[(@resource-id = 'org.wikipedia:id/page_list_item_container')]")).size();
+        if (c >= 2) {
+            waitForElementAndClick(
+                    By.xpath("//*[(@resource-id = 'org.wikipedia:id/search_close_btn')]"),
+                    "can't find X ",
+                    10
+            );
+            //не работает этот метод. Ошибка:
+            // INFO: HTTP Status: '405' -> incorrect JSON status mapping for 'unknown error' (500 expected)
+            //org.openqa.selenium.WebDriverException: Method is not implemented
+            // Не понимаю почему, т.к.метод идентичен методу урока.
+            waitForElementNotPresent(
+                    By.xpath("//*[(@resource-id = 'org.wikipedia:id/page_list_item_container')]"),
+                    "Search result is still present",
+                    10
+            );
+        } else {
+            System.out.println("The search has not given any results");
+        }
+    }
+
+    private WebElement waitForElementPresents(By by, String error_massage, long timeoutSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSeconds);
         wait.withMessage(error_massage + "\n");
         return wait.until(
-                ExpectedConditions.presenceOfElementLocated(by)
+                presenceOfElementLocated(by)
         );
     }
 
-    private WebElement waitForElementPresents(By by, String error_massage)
-    {
+    private WebElement waitForElementPresents(By by, String error_massage) {
         return waitForElementPresents(by, error_massage, 5);
     }
 
-    private WebElement waitForElementAndClick(By by, String error_massage, long timeoutSeconds)
-    {
+    private WebElement waitForElementAndClick(By by, String error_massage, long timeoutSeconds) {
         WebElement element = waitForElementPresents(by, error_massage, 5);
         element.click();
         return element;
     }
 
-    private WebElement waitForElementAndSendKeys(By by, String value, String error_massage, long timeoutSeconds)
-    {
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_massage, long timeoutSeconds) {
         WebElement element = waitForElementPresents(by, error_massage, 5);
         element.sendKeys(value);
         return element;
+    }
+
+    private boolean waitForElementNotPresent(By by, String error_massage, long timeOutSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutSeconds);
+        wait.withMessage(error_massage + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
     }
 
 }
